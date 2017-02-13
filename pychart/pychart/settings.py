7dd096776ff85ecf4744790ffa268d014d4bc660
Ted Callahan <CCallahanIV@gmail.com>
2017-02-13 12:06:10 -0800

    Created render/ directory in MEDIA.  Moved demo files in there.

--- pychart/pychart/settings.py
+++ pychart/pychart/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pychart',
-    # 'pychart_profile',
-    # 'pychart_datarender',
+    'pychart_profile',
+    'pychart_datarender',
 ]
 
 MIDDLEWARE = [
