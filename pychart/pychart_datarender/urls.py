2279cc6ca4fc253fb795f98f7dbe01b9ad6bf801
jordan <j.schatzman@outlook.com>
2017-02-13 13:21:41 -0800

    adding URLS and models to admin

--- pychart/pychart_datarender/urls.py
+++ pychart/pychart_datarender/urls.py
@@ -1,6 +1,35 @@
 """Url patterns for data render app."""
 
 from django.conf.urls import url
-from imager_profile.views import ProfileView, EditProfileView
+#from imager_profile.views import ProfileView, EditProfileView
+from datarender.views import (
+    GalleryView,
+    DataDetailView,
+    RenderDetailView,
+    DataLibraryView,
+    EditDataView,
+    EditRenderView,
+    AddDataView,
+    AddRenderView
+)
+
+urlpatterns = [
+
+    url(r'gallery/$', GalleryView.as_view(), name='gallery'),
+    url(r'^data/(?P<pk>\d+)/$', DataDetailView.as_view(), name='data_detail'),
+    url(r'^render/(?P<pk>\d+)/$', RenderDetailView.as_view(), name='render_detail'),
+    url(r'^data/(?P<pk>\d+)/edit/$', EditDataView.as_view(), name='data_edit'),
+    url(r'^render/(?P<pk>\d+)/edit/$', EditRenderView.as_view(), name='render_edit'),
+    url(r'^data/(?P<pk>\d+)/add/$', AddDataView.as_view(), name='data_add'),
+    url(r'^render/(?P<pk>\d+)/add/$', AddRenderView.as_view(), name='render_add')
+
+]
 
 
+
+
+
+
+
+
+]
