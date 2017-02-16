b0c2d7dfd6f4c21eb3361226f9702e42423d87bb
Ted Callahan <CCallahanIV@gmail.com>
2017-02-16 13:36:01 -0800

    reconfigured settings.py for deployment.

--- pychart/pychart/settings.py
+++ pychart/pychart/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -138,11 +138,11 @@ LOGIN_REDIRECT_URL = 'pychart_profile:profile'
 
 # Email setup
 ACCOUNT_ACTIVATION_DAYS = 7
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'thepychart@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
